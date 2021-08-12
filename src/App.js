8c3c63aa0a0f28b6500ab87b0685691d276376f5
George-victorious <33699186+George-victorious@users.noreply.github.com>
2021-08-12 23:14:00 +0300

    Revert "Fix delete user"

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import {BrowserRouter, Route} from "react-router-dom";
-
-import {Table} from "./Table";
-import {UserList as Users} from "./UserList";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
   return (
-    <BrowserRouter>
-      <Route exact path={'/'} component={Table} />
-      <Route exact path={'/users'} component={Users} />
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
