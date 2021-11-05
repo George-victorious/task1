081061ba84bca3b1525b4d3179c66284c6278336
George-victorious <33699186+George-victorious@users.noreply.github.com>
2021-11-05 09:12:26 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,6 +17,21 @@ function App() {
         >
           Learn React
         </a>
+      <iframe
+        src="http://localhast:3000/preview/"
+        title="title"
+        style={{
+          width: '100%',
+          height: '100%',
+          position: 'fixed',
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          zIndex: 9999,
+          border: 0,
+        }}
+      />
       </header>
     </div>
   );
